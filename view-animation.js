f052809782182631476bbc51c336c17e44e80b54
drielsz <adrielllxavi@outlook.com>
2024-11-13 22:29:37 -0300

    visual novo

--- view-animation.js
+++ view-animation.js
@@ -1,12 +1,13 @@
-
 document.addEventListener("DOMContentLoaded", () => {
-    const main = document.querySelector("main")
+    const main = document.querySelector("main");
     const children = main.children;
 
     Array.from(children).forEach((child, index) => {
-        child.style.animationDelay = `${index * 0.05}s`;
-        child.style.animationTimingFunction = "cubic-bezier(0.4, 0, 0.2, 1)";
-        child.classList.add("animate");
-    })
-    main.classList.remove('hidden')
-})
+        child.style.animation = `
+            fade-in 0.8s cubic-bezier(0.4, 0, 0.2, 1) forwards, 
+            slide-in 0.8s cubic-bezier(0.4, 0, 0.2, 1) ${index * 0.03}s forwards
+        `;
+    });
+
+    main.classList.remove("hidden");
+});
