94d92653f48ef538cf1e7cbe1af4379f0264ea5b
drielsz <adrielllxavi@outlook.com>
2024-11-13 22:01:49 -0300

    visual novo

--- view-animation.js
+++ view-animation.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const children = main.children;
 
     Array.from(children).forEach((child, index) => {
-        child.style.animationDelay = `${index * 0.03}s`;
+        child.style.animationDelay = `${index * 0.05}s`;
         child.style.animationTimingFunction = "cubic-bezier(0.4, 0, 0.2, 1)";
         child.classList.add("animate");
     })
