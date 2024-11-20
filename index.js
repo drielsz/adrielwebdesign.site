1f64dcbd611839dc7049b483ad7ecaca1891e570
drielsz <adrielllxavi@outlook.com>
2024-11-20 00:20:29 -0300

    tema

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const dropdown = document.querySelector(".dropdown");
 const boxes = document.querySelectorAll(".container-card-projetos");
 
 
-
 hamburguer.addEventListener("click", () => {
   hamburguer.classList.toggle("active");
   navMenu.classList.toggle("active");
@@ -71,3 +70,49 @@ const obterCurtidasPorPost = () => {
 };
 // Chame essa função ao carregar a página
 obterCurtidasPorPost();
+
+
+const SelectThemeButtons = document.querySelectorAll('.select-theme');
+
+SelectThemeButtons.forEach((selectbutton) => {
+  selectbutton.addEventListener("click", () => {
+    const appearIcons = document.querySelectorAll('.appear-icon')
+    const hiddenIcons = document.querySelectorAll('.hidden-icon');
+    const appearIconsSelect = document.querySelectorAll('.appear-icon-select');
+    const hiddenIconSelect = document.querySelectorAll('.hidden-icon-select')
+    
+    document.body.classList.toggle("dark")
+    document.body.classList.toggle("white")
+    const currentTheme = body.classList.contains('dark') ? 'dark' : 'white';
+    localStorage.setItem('theme', currentTheme)
+    // Alterna as classes entre os ícones escondidos e visíveis
+
+
+    appearIcons.forEach((appearicon) => {
+      appearicon.classList.toggle('appear-icon');
+      appearicon.classList.toggle('hidden-icon');
+    });
+    hiddenIcons.forEach((hiddenicon) => {
+      hiddenicon.classList.toggle('hidden-icon');
+      hiddenicon.classList.toggle('appear-icon');
+      hiddenicon.classList.add('animateVerticalSlideIcon');
+    });
+    
+    appearIconsSelect.forEach((appearicon) => {
+      appearicon.classList.toggle('appear-icon-select');
+      appearicon.classList.toggle('hidden-icon-select');
+    });
+    hiddenIconSelect.forEach((hiddeniconselect) => {
+      hiddeniconselect.classList.toggle('hidden-icon-select');
+      hiddeniconselect.classList.toggle('appear-icon-select');
+    })
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedTheme = localStorage.getItem('theme')
+  if (savedTheme) {
+    body.classList.remove('dark', 'white')
+    body.classList.add(savedTheme)
+  }
+})
